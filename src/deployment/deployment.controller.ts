d0dbaeccc93079e3ac13d00e0eb2ab9c36194449
Shamil <78428735+sshme@users.noreply.github.com>
2025-02-19 23:45:38 +0300

    Universal gateway for different frontentends (#2)
    * nest-auth-gateway
    
    * all-methods-in-auth-service-return-promise
    
    * auth-logic-update
    
    * deployment-servers-managing-endpoints
    
    * format
    
    ---------
    
    Co-authored-by: Sham <sham@MacBook-Air-Sham.local>

---
+++ src/deployment/deployment.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { DeploymentService } from './deployment.service';
+import {
+  AddServerRequest,
+  ListServersRequest,
+  RemoveServerRequest,
+} from 'protos/gen/ts/deployment/deployment';
+import {
+  AddServerRequestDto,
+  AddServerResponseDto,
+  ListServersResponseDto,
+  RemoveServerResponseDto,
+} from './dto/base';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+
+@Controller('deployment')
+export class DeploymentController {
+  private readonly logger = new Logger(DeploymentController.name);
+
+  constructor(private readonly deploymentService: DeploymentService) {}
+
+  @Get('servers')
+  @ApiOperation({ summary: 'List all servers' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of servers',
+    type: ListServersResponseDto,
+  })
+  async getAllServers(@Req() req: ListServersRequest) {
+    this.logger.log(`List servers request`);
+    return await this.deploymentService.getAllServers(req);
+  }
+
+  @Post('servers')
+  @ApiOperation({ summary: 'Add a new server' })
+  @ApiBody({ type: AddServerRequestDto })
+  @ApiResponse({
+    status: 200,
+    description: 'The added server',
+    type: AddServerResponseDto,
+  })
+  async addServer(@Body() req: AddServerRequest) {
+    this.logger.log(`Add server request: ${JSON.stringify(req)}`);
+    return await this.deploymentService.addServer(req);
+  }
+
+  @Delete('servers/:id')
+  @ApiOperation({ summary: 'Remove a server' })
+  @ApiParam({ name: 'id', type: Number, description: 'Server ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Status of the remove operation',
+    type: RemoveServerResponseDto,
+  })
+  async removeServer(@Param('id') id: number) {
+    this.logger.log(`Remove server request: ${id}`);
+    const req: RemoveServerRequest = { id };
+    return await this.deploymentService.removeServer(req);
+  }
+}
---
+++ src/deployment/dto/base.ts
@@ -0,0 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ServerPreviewDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: '192.168.1.1' })
+  ip: string;
+
+  @ApiProperty({ example: 22 })
+  port: number;
+
+  @ApiProperty({ example: 'admin' })
+  user: string;
+}
+
+export class AddServerRequestDto {
+  @ApiProperty({ example: '192.168.1.1' })
+  ip: string;
+
+  @ApiProperty({ example: 22 })
+  port: number;
+
+  @ApiProperty({ example: 'admin' })
+  user: string;
+
+  @ApiProperty({ example: 'password' })
+  password: string;
+}
+
+export class ListServersRequestDto {}
+
+export class RemoveServerRequestDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+}
+
+export class RemoveServerResponseDto {
+  @ApiProperty({ example: true })
+  status: boolean;
+}
+
+export class ListServersResponseDto {
+  @ApiProperty({ type: [ServerPreviewDto] })
+  servers: ServerPreviewDto[];
+}
+
+export class AddServerResponseDto {
+  @ApiProperty({ type: ServerPreviewDto, nullable: true })
+  server: ServerPreviewDto | undefined;
+}
